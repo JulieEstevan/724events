ef306ea30e08a45ca67939964c18e030e0333078
JulieEstevan <julie.s.estevan@gmail.com>
2024-06-04 15:02:12 +0200

    adding last for the latest event in data, correcting event card in home page

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -4,6 +4,7 @@ import {
   useCallback,
   useContext,
   useEffect,
+  useMemo,
   useState,
 } from "react";
 
@@ -19,6 +20,17 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+  // Ajout afin de stocker l'event le plus recent
+  const [last, setLast] = useState(null);
+
+// Ajout afin de surveiller et trier data pour definir last selon l'event le plus recent 
+useEffect(() => {
+  if (data && data.events) {
+    const sortedEvents = data.events.sort((a, b) => new Date(b.date) - new Date(a.date))
+    setLast(sortedEvents[0])
+  }
+}, [data])
+
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
@@ -31,14 +43,12 @@ export const DataProvider = ({ children }) => {
     getData();
   });
   
+const providerValue = useMemo(() => ({
+  data, error, last
+}), [data, error, last])
+
   return (
-    <DataContext.Provider
-      // eslint-disable-next-line react/jsx-no-constructed-context-values
-      value={{
-        data,
-        error,
-      }}
-    >
+    <DataContext.Provider value={providerValue}>
       {children}
     </DataContext.Provider>
   );
